42e99c919dc84d109ab108ea7620929f6dc3996a
Wook <refoz0721@gmail.com>
2020-07-22 10:01:19 +0900

    test

--- app/src/main/java/com/jwook/gabaekseo/InfoActivity.java
+++ app/src/main/java/com/jwook/gabaekseo/InfoActivity.java
@@ -3,6 +3,7 @@ package com.jwook.gabaekseo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,10 +11,14 @@ public class InfoActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+
+
 
 
 
--- app/src/main/java/com/jwook/gabaekseo/MyRecyclerAdapter.java
+++ app/src/main/java/com/jwook/gabaekseo/MyRecyclerAdapter.java
@@ -60,6 +60,8 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter {
 
 
 
+
+
             }
         });
 
