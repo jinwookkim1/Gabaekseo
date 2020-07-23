ed2bb2089d378817098baa964a82c4da8fac9ab5
Wook <refoz0721@gmail.com>
2020-07-23 09:33:44 +0900

    test

--- app/src/main/java/com/jwook/gabaekseo/InfoActivity.java
+++ app/src/main/java/com/jwook/gabaekseo/InfoActivity.java
@@ -1,17 +1,23 @@
 package com.jwook.gabaekseo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -23,6 +29,7 @@ public class InfoActivity extends AppCompatActivity {
     TextView tvTel;
     TextView tvTime;
     GoogleMap Gmap;
+    SearchView searchView;
 
 
     @Override
@@ -44,6 +51,7 @@ public class InfoActivity extends AppCompatActivity {
         String url = intent.getStringExtra("url");
         final double x = intent.getDoubleExtra("x", 37.556355);
         final double y = intent.getDoubleExtra("y", 126.977929);
+        Toast.makeText(this, x+", "+y, Toast.LENGTH_SHORT).show();
 
         Glide.with(this).load(url).into(iv);
         tvName.setText(name);
@@ -61,6 +69,14 @@ public class InfoActivity extends AppCompatActivity {
                 LatLng place = new LatLng(x, y);
                 MarkerOptions markerOptions = new MarkerOptions();
                 markerOptions.position(place);
+                markerOptions.title("Here");
+                Gmap.addMarker(markerOptions);
+
+                Gmap.animateCamera(CameraUpdateFactory.newLatLngZoom(place,15));
+                UiSettings settings = Gmap.getUiSettings();
+                settings.setZoomControlsEnabled(true);
+
+
             }
         });
 
@@ -71,6 +87,32 @@ public class InfoActivity extends AppCompatActivity {
 
 
 
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+        MenuItem item = menu.findItem(R.id.bar_search);
+        searchView = (SearchView)item.getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("검색어를 입력하세요");
+        searchView.setQuery("",false);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(InfoActivity.this, query+"를 검색하였습니다.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
 
     }
